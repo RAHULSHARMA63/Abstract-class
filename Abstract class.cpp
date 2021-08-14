2fac78fc8361a9d2b323ad5d600c73b3c0c0ac28
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-08-14 11:18:28 +0530

    To show the Abstract class.

---
+++ Abstract class.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Animal{
+public:   
+   virtual void sound() = 0;   
+   void sleeping() {
+      cout<<"Sleeping";
+   }
+};
+class Dog: public Animal{
+public:
+   void sound() {
+      cout<<"Woof"<<"\n";
+   }
+};
+int main()
+{
+   Dog obj;
+   obj.sound();
+   obj.sleeping();
+   return 0;
+}
